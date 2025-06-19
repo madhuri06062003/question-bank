3620709a54c2b068b77becba8b2c3f162e2a76f7
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2025-06-19 10:58:57 +0530

    a

---
+++ swap_2_with_extra_var.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    int a,b,var;
+    printf("Enter 2 numbers :");
+    scanf("%d %d",&a,&b);
+    printf("Enter variable : \n");
+    scanf("%d",&var);
+    var = a;
+    a=b;
+    b=var;
+    printf("After swapping :\n");
+    printf("a=%d b=%d",a,b);
+    return 0;
+}
