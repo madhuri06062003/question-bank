80d352116c2003777d169d903bee411602e99f22
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2025-06-19 13:11:27 +0530

    a

--- output.c
+++ output.c
@@ -13,3 +13,19 @@ Printf(“Are You”);
 
 Explanation : Here else part is skipped because if is defined VIVEN
 output : HelloAre You
+
+
+What is the output of the below seudocode?
+
+#include <stdio.h>
+#include <string.h>
+int main() {
+int nu = 10;
+int val;
+memcpy(&val, nu, sizeof(int));
+printf("val = %d\n", val);
+return 0; 
+}
+
+Explanation : It gets error because in memcpy &nu is must
+Output : Error
