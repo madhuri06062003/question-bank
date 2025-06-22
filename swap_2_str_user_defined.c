df4ae28e77be492b31a93845b339b39eef77504b
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2025-06-22 14:56:26 +0530

    a

---
+++ swap_2_str_user_defined.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// User-defined function to check if two strings are equal
+int areStringsEqual(char str1[], char str2[]) {
+    int i = 0;
+
+    // Compare each character
+    while (str1[i] != '\0' && str2[i] != '\0') {
+        if (str1[i] != str2[i]) {
+            return 0; // Strings are not equal
+        }
+        i++;
+    }
+
+    // If both strings end at the same time, they are equal
+    if (str1[i] == '\0' && str2[i] == '\0') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    char str[] = "viven";
+    char str1[] = "viven";
+
+    if (areStringsEqual(str, str1)) {
+        printf("Two strings are equal\n");
+    } else {
+        printf("Two strings are not equal\n");
+    }
+
+    return 0;
+}
