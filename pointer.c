5e713574ea1c7704b06dc9ae7c2f148647911cc0
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2025-06-23 11:50:18 +0530

    a

---
+++ pointer.c
@@ -0,0 +1,14 @@
+/*Pointer : Stores memory address of another variable
+Example:*/
+#include<stdio.h>
+int main() {
+    int a=10;
+   int *ptr=&a;
+
+    printf("Value of a :%d\n",a);
+    printf("Value of ptr :%p\n",ptr);
+    printf("Address of a :%p\n",&a);
+    printf("Address of ptr :%d\n",*ptr);
+
+    return 0;
+}
