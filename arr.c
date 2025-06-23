7eead543af9d0edf934041887cc5c2c17b425920
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2025-06-23 11:44:45 +0530

    a

---
+++ arr.c
@@ -0,0 +1,11 @@
+/*Array : collection of elements of same data that stored in memory
+Example:*/
+#include<stdio.h>
+int main() {
+    int arr[]={1,2,3,4,5,6,7,8,9};
+    int i;
+    for(i=0;i<=9;i++){
+    printf("%d index[%d]",i,arr[i]);
+    }
+    return 0;
+}
